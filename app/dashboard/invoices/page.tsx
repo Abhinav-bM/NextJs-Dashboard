58c47d2fe84188fdfd503f3c4b5db8727876799b
Abhinav-bM <abhinavbmanoj@gmail.com>
2024-11-18 09:58:14 +0530

    update

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      invoice page is here
+    </div>
+  )
+}
+
+export default page
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+
+const Page = () => {
+  return (
+   
+   <p>Dashboard page is here 
+   </p>
+  )
+}
+
+export default Page
